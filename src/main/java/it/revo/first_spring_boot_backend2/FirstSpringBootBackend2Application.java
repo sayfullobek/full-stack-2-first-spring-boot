eb7c32ec5af49ffb40cadbe8ab8a12345cbbbbbd
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-05-16 05:55:07 -0700

    sayfullo

--- src/main/java/it/revo/first_spring_boot_backend2/FirstSpringBootBackend2Application.java
+++ src/main/java/it/revo/first_spring_boot_backend2/FirstSpringBootBackend2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstSpringBootBackend2Application {
     public static void main(String[] args) {
+        System.out.println("salom git hub");
         SpringApplication.run(FirstSpringBootBackend2Application.class, args);
     }
 }
